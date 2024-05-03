413cb556d346d7b6b73bcae11a8e3a344b95d188
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-05-03 14:44:13 -0700

    2-execute_a_command.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# Installing Flask from pip3
+# Installing Flask, werkzeug from pip3
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => installed,
   provider => 'pip3',
 }
 
